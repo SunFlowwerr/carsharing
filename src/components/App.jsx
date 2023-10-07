0305e59f89e7eabb64a5f1876dd2be7097cd97cd
SkliarovaAnastasiia <skliarova16@gmail.com>
2023-10-08 00:05:31 +0300

    added components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,10 +4,6 @@ export const App = () => {
       style={{
         height: '100vh',
         display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
       }}
     >
       React homework template
