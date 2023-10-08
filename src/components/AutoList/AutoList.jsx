ec0e6aab24ac93c49c32de4712b91c5891505d60
SkliarovaAnastasiia <skliarova16@gmail.com>
2023-10-08 11:42:41 +0300

    Update AutoList.jsx

--- src/components/AutoList/AutoList.jsx
+++ src/components/AutoList/AutoList.jsx
@@ -18,57 +18,5 @@ export const AutoList = () => {
 
   const arrayOfCars = cars.slice(0, counter);
 
-  return (
-    <div className={css.page}>
-      <ul className={css.listOfCars}>
-        {arrayOfCars ? (
-          arrayOfCars.map(car => (
-            <li key={nanoid()} className={css.car}>
-              <div
-                className={css.imgContainer}
-                style={{
-                  backgroundImage: `url(${car.img})`,
-                  backgroundRepeat: 'no-repeat',
-                  backgroundPosition: 'center',
-                  backgroundSize: 'cover',
-                  width: 274,
-                  height: 268,
-                  borderRadius: 14,
-                }}
-              >
-                <button>Heart</button>
-              </div>
-              <div className={css.textContainer}>
-                <p className={css.title}>
-                  {car.make}
-                  <span className={css.span}> {car.model}</span>, {car.year}
-                </p>
-                <p className={css.price}>{car.rentalPrice}</p>
-              </div>
-              <div>
-                <div className={css.infoContainer}>
-                  <p style={{ margin: 0 }}>
-                    {car.address.split(',').slice(1, 2)} |
-                    {car.address.split(',').slice(2, 3)} | {car.rentalCompany} |{' '}
-                    {car.model} | {car.id} | {car.functionalities.slice(0, 1)}
-                  </p>
-                </div>
-              </div>
-              {/* <button
-                onClick={() => toggleModal(car)}
-                className={css.learnMoreBtn}
-              >
-                Learn more
-              </button> */}
-            </li>
-          ))
-        ) : (
-          <p>Loading</p>
-        )}
-      </ul>
-      <button onClick={() => handleLoader()} className={css.loadMoreBtn}>
-        Load more
-      </button>
-    </div>
-  );
+  return <div></div>;
 };
