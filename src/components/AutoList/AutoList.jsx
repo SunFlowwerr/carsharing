193e8dc4b81be05c7b8de7581001bc894bf77833
SkliarovaAnastasiia <skliarova16@gmail.com>
2023-10-08 12:35:50 +0300

    summary

--- src/components/AutoList/AutoList.jsx
+++ src/components/AutoList/AutoList.jsx
@@ -1,9 +1,3 @@
-import { useEffect, useState } from 'react';
-import { fetchCars } from 'redux/operations';
-import useLocalStorage from 'hooks/useLocalStorage';
-import { nanoid } from 'nanoid';
-import css from './AutoList.module.css';
-
 export const AutoList = () => {
-  return <div className={css.page}></div>;
+  return <div></div>;
 };
