a419d57f9e7c88b5509f30fdcba4667d8930b2f0
SkliarovaAnastasiia <skliarova16@gmail.com>
2023-10-08 11:54:55 +0300

    summary

--- src/redux/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import { carsReducer } from './carsSlice';
-
-export const store = configureStore({
-  reducer: {
-    cars: carsReducer,
-  },
-});
