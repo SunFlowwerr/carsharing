442a62b6e6c3aa1720026d46e524204023f1e6ab
SkliarovaAnastasiia <skliarova16@gmail.com>
2023-10-08 11:00:03 +0300

    Update AutoList.jsx

--- src/components/AutoList/AutoList.jsx
+++ src/components/AutoList/AutoList.jsx
@@ -3,7 +3,6 @@ import { fetchCars } from 'redux/operations';
 import useLocalStorage from 'hooks/useLocalStorage';
 import { nanoid } from 'nanoid';
 import css from './AutoList.module.css';
-// import { Modal } from '../Modal/Modal';
 
 export const AutoList = () => {
   const { dispatch, cars } = useLocalStorage();
@@ -78,7 +77,6 @@ export const AutoList = () => {
       <button onClick={() => handleLoader()} className={css.loadMoreBtn}>
         Load more
       </button>
-      {/* {isModalOpen && <Modal onClose={toggleModal} car={currentCar}></Modal>} */}
     </div>
   );
 };
