9eab502dbc405f759fc0fce31303eb089aac64cc
SkliarovaAnastasiia <skliarova16@gmail.com>
2023-10-08 12:39:24 +0300

    Update AutoList.jsx

--- src/components/AutoList/AutoList.jsx
+++ src/components/AutoList/AutoList.jsx
@@ -1,3 +1,9 @@
+import css from './AutoList.module.css';
+import { useState, useEffect } from 'react';
+import useLocalStorage from 'hooks/useLocalStorage';
+import { fetchCars } from 'redux/operations';
+import { nanoid } from 'nanoid';
+
 export const AutoList = () => {
   return <div></div>;
 };
